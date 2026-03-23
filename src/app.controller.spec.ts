e2d05347f2a956a911d58cfa057d784e31ce4306
Adel Obaji <adelobaji23@gmail.com>
2026-03-23 10:31:53 -0900

    fix test file

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -9,7 +9,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World"', () => {
       expect(appController.getHello()).toBe('Hello World');
     });
   });
